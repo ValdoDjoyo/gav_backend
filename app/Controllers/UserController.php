e650482c4f5c48845097bb6a13c918b8c57c814c
valdo <djoyovaldo@gmail.com>
2022-09-06 00:29:49 +0200

    first commit

---
+++ app/Controllers/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\API\ResponseTrait;
+use App\Models\UserModel;
+use \Firebase\JWT\JWT;
+
+class UserController extends BaseController
+{
+    use ResponseTrait;
+
+    public function login()
+    {
+        $userModel = new UserModel();
+
+        $login = $this->request->getVar('login');
+        $password = $this->request->getVar('password');
+
+        $user = $userModel->where('login', $login)->first();
+
+        if (is_null($user)) {
+            return $this->respond(['error' => 'Invalid username or password.'], 401);
+        }
+
+        $pwd_verify = password_verify($password, $user['password']);
+
+        if (!$pwd_verify) {
+            return $this->respond(['error' => 'Invalid username or password.'], 401);
+        }
+
+        $key = getenv('JWT_SECRET');
+        $iat = time(); // current timestamp value
+        $exp = $iat + 3600;
+
+        $payload = array(
+            "iss" => "Issuer of the JWT",
+            "aud" => "Audience that the JWT",
+            "sub" => "Subject of the JWT",
+            "iat" => $iat, //Time the JWT issued at
+            "exp" => $exp, // Expiration time of token
+            "login" => $user['login'],
+        );
+
+        $token = JWT::encode($payload, $key, 'HS256');
+
+        $response = [
+            'message' => 'Login Succesful',
+            'token' => $token
+        ];
+
+        return $this->respond($response, 200);
+    }
+    public function register()
+    {
+        $rules = [
+            'email' => ['rules' => 'required|min_length[4]|max_length[255]|valid_email|is_unique[users.email]'],
+            'password' => ['rules' => 'required|min_length[8]|max_length[255]'],
+            'confirm_password'  => ['label' => 'confirm password', 'rules' => 'matches[password]']
+        ];
+
+
+        if ($this->validate($rules)) {
+            $model = new UserModel();
+            $data = [
+                'email'    => $this->request->getVar('email'),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+            ];
+            $model->save($data);
+
+            return $this->respond(['message' => 'Registered Successfully'], 200);
+        } else {
+            $response = [
+                'errors' => $this->validator->getErrors(),
+                'message' => 'Invalid Inputs'
+            ];
+            return $this->fail($response, 409);
+        }
+    }
+    public function getUser()
+    {
+        $users = new UserModel;
+        return $this->respond(['users' => $users->findAll()], 200);
+    }
+}
---
+++ app/Database/Migrations/2022-09-05-152629_AddUser.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddUser extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'BIGINT',
+                'constraint' => 255,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'login' => [
+                'type' => 'VARCHAR',
+                'unique' => true,
+                'constraint' => '255',
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'ville' => [
+                'type' => 'VARCHAR',
+                'constraint' => '40',
+            ],
+            'localite' => [
+                'type'=> 'VARCHAR',
+                'constraint' => 40
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true
+            ],
+        ]);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
